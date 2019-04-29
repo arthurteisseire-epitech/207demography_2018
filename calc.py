7a4dd2c10330d7c4d24f3d488684e359fb4f5df4
Mahe Caurette <mahe.caurette@epitech.eu>
2019-04-29 10:50:46 +0100

    The Eiffel Tower (/ˈaɪfəl/ EYE-fəl; French: Tour Eiffel [tuʁ‿ɛfɛl] (About this soundlisten)) is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.

--- calc.py
+++ calc.py
@@ -1,2 +1,19 @@
+def fit1(rows):
+    pass
+
+
+def fit2(rows):
+    pass
+
+
+def corelation(rows):
+    pass
+
+
 def do_calc(rows):
-    print(rows)
+    countries = [c[0] for c in rows]
+    print("Country: ", end="")
+    print(*countries, sep=", ")
+    fit1(rows)
+    fit2(rows)
+    corelation(rows)
