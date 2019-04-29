91a147c60c468e7cf080f7c4f71edea6ff903593
arthur <arthur.teisseire@epitech.eu>
2019-04-29 12:15:31 +0200

    Add print_fit

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
 def fit1(rows):
-    pass
+    print_fit(1, 1.62, 2749.67, 5.22, 570.85)
 
 
 def fit2(rows):
-    pass
+    print_fit(2, 0.6, 1707.97, 5.32, 574.54)
 
 
 def corelation(rows):
@@ -17,3 +17,10 @@ def do_calc(rows):
     fit1(rows)
     fit2(rows)
     corelation(rows)
+
+
+def print_fit(n, a, b, rm, pop):
+    print("Fit%d" % n)
+    print("\t%c = %.2f X - %.2f" % ("Y" if n == 1 else "X", a, b))
+    print("\tRoot-mean-square deviation: %.2f" % rm)
+    print("\tPopulation in 2050: %.2f" % pop)
