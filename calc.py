28056b9b966abcd9e3c3183a41c540f727daae4b
Mahe Caurette <mahe.caurette@epitech.eu>
2019-05-02 10:35:20 +0100

    using reals years

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
-def fit1(rows):
-    ab = ax_b(rows)
+def fit1(years, rows):
+    ab = ax_b(years, rows)
     print_fit(1, ab[0], ab[1], 5.32, 574.54)
     pass
 
@@ -15,7 +15,7 @@ def corelation(rows):
 def do_calc(countries, years, values):
     print("Country: ", end="")
     print(*countries, sep=", ")
-    fit1(values[0])
+    fit1(years, values[0])
     fit2(values)
     corelation(values)
 
@@ -27,12 +27,16 @@ def print_fit(n, a, b, rm, pop):
     print("\tPopulation in 2050: %.2f" % pop)
 
 
-def ax_b(rows):
-    year = list(range(1960, 2018))
-    year_sum = sum(year)
-    year_2_sum = sum(map(lambda x: x * x, year))
-    pop_sum = sum(rows)
-    pop_year_sum = sum(map(lambda x: x[0] * x[1], zip(year, rows)))
-    b = (pop_sum * year_2_sum - year_sum * pop_year_sum) / (len(year) * year_2_sum - pow(year_sum, 2)) / 1000000
-    a = (len(year) * pop_year_sum - year_sum * pop_sum) / (len(year) * year_2_sum - pow(year_sum, 2)) / 1000000
+def ax_b(years, val):
+    year_sum = sum(years)
+    year_2_sum = sum(map(lambda x: x * x, years))
+    pop_sum = sum(val)
+    pop_year_sum = sum(map(lambda x: x[0] * x[1], zip(years, val)))
+    b = (pop_sum * year_2_sum - year_sum * pop_year_sum) / (len(years) * year_2_sum - pow(year_sum, 2)) / 1000000
+    a = (len(years) * pop_year_sum - year_sum * pop_sum) / (len(years) * year_2_sum - pow(year_sum, 2)) / 1000000
     return a, b
+
+
+def tab_mean(tab):
+    pass
+
